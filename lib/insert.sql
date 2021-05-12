9a4aa02a393c427349ea3a8d62c5cdb6bd46a549
Daysha Jones <daysha.vu@icloud.com>
2021-05-12 20:39:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,19 @@
-INSERT INTO users (id, name, age) VALUES (1, 'Daysha', 28), (2, 'Des', 28), (3, 'Timm', 35), (4, 'Valyn', 2), (5, 'Greg', 32), (6, 'Debi', 54), (7, 'Jeff', 53), (8, 'Chris', 32), (9, 'Masha', 33), (10, 'Kelly', 26), (11, 'Penny', 6), (12, 'Ziggy', 4), (13, 'Lady', 7), (14, 'Duke', 5), (15, 'Jenny', 42), (16, 'Lynda', 73), (17, 'Richard', 76), (18, 'Brenda', 58), (19, 'Kyle', 32), (20, 'Dilly', 6);
+INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
+(5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
+(11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
+(17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
 
-INSERT INTO projects (title, category, funding_goal, start_data, end_date) VALUES (1, "Help me with school", "School", 5000, 10/01/2020, 10/01/2021), (2, "I need a car", "Car", 10000, 5/19/2021, 5/19/2022 ), (3, "Jeep fund", "Car", 6000, 2/2/2021, 2/2/2022), (4, "Soccer Camp", "Sports", 150, 5/1/2021, 5/1/2022), (5, "Animal Shelter fund", "animals", 1000, 1/1/2021, 1/1/2022), (6, "Travel fund", "travel", 3000, 11/11/2021, 11/11/2022), (7, "Music Class", "Music", 200, 8/1/2021, 8/1/2022), (8, "College fund", "college", 5/20/2030, 5/20/2034), (9, "Baby fund", "family", 20000, 9/1/2021, 9/1/2022), (10, "Open coffee shop", "business", 8/20/2021, 8/20/2022);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+(2, 'My book on SQL', 'books', 20.00, '2013-01-01', '2014-01-01'),
+(3, 'The next Harry Potter', 'books', 1000.00, '2013-05-30', '2015-05-30'),
+(4, 'Animal shelter needs dog food', 'charity', 400.00, '2013-03-14', '2013-06-30'),
+(5, 'Voldement needs a body', 'charity', 6000.00, '2013-03-20', '2013-09-20'),
+(6, 'The next Inna-Gadda-Davida', 'music', 200.00, '2014-12-30', '2015-12-30'),
+(7, 'Iguana needs tail operation', 'charity', 2000.00, '2013-10-02', '2013-10-30'),
+(8, 'I have bed bugs!', 'charity', 800.00, '2014-06-30', '2014-06-31'),
+(9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
+(10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (1, 10.00, 1, 2),
